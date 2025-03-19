8efd34f9433787a56b1bb9b830bfd176759e79b7
Ataklti Nguse <34140875+bbcnewsataklti@users.noreply.github.com>
2025-03-19 10:35:14 +0300

    Add files via upload
    Assignment one for Python

---
+++ intropython.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        result = num1 / num2
+else:
+    print("Invalid operation.")
+print(f"The result is: {result}")
